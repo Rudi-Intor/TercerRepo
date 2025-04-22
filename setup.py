8a69ddb399a2137672c29144ca652239ebbee5a5
rudi Intor <rudiintor123@gmail.com>
2025-04-22 18:18:50 -0500

    guardando archivos  tercerrepo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TercerRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Rudi-Intor",                         # Tu nombre
+    author_email="rudiintor123@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/Rudi-Intor/TercerRepo",     # URL del proyecto
+)
